0e78cfeb59cda105bec367902a66cf106f04214c
maham <mahamsajid450@gmail.com>
2025-04-27 17:22:46 +0200

    cambiar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Handle tab switching
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -5,11 +6,20 @@ function opentab(tabname, event) {
    for (let tablink of tablinks) {
         tablink.classList.remove('active-link');
    }
-
    for (let tabcontent of tabcontents) {
       tabcontent.classList.remove('active-tab');
    }
-
    event.currentTarget.classList.add("active-link");
    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Handle sidebar menu open/close
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+   sidemenu.style.right = "0";
+}
+
+function closemenu() {
+   sidemenu.style.right = "-200px";
 }

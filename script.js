e59b1a0ef8ca874cd9dc503b63aa0289c7391078
maham <mahamsajid450@gmail.com>
2025-04-30 13:55:19 +0200

    cambios estilos

--- script.js
+++ script.js
@@ -3,23 +3,26 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname, event) {
-   for (let tablink of tablinks) {
-        tablink.classList.remove('active-link');
-   }
-   for (let tabcontent of tabcontents) {
-      tabcontent.classList.remove('active-tab');
-   }
-   event.currentTarget.classList.add("active-link");
-   document.getElementById(tabname).classList.add("active-tab");
+  // Remove active class from all tabs and contents
+  for (let tablink of tablinks) {
+    tablink.classList.remove('active-link');
+  }
+  for (let tabcontent of tabcontents) {
+    tabcontent.classList.remove('active-tab');
+  }
+
+  // Add active class to clicked tab and matching content
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
 
 // Handle sidebar menu open/close
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu() {
-   sidemenu.style.right = "0";
+  sidemenu.style.right = "0";
 }
 
 function closemenu() {
-   sidemenu.style.right = "-200px";
+  sidemenu.style.right = "-200px";
 }
